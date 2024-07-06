0af02bdc585bdbe3f90788bd4b1bc8db9e1cdf43
maniziva-manikandan <adaikalam.manikandan@moneyforward.co.jp>
2024-07-06 17:34:51 +0530

    Tax Rounding added
    -Tax Rounding
    -TaxRounding Page
    -Radio assert change

--- src/pages/Settings/Default_claim_deposit_accounts_Tax.page.ts
+++ src/pages/Settings/Default_claim_deposit_accounts_Tax.page.ts
@@ -4,6 +4,9 @@ const baseURL = process.env.BASEURL as string;
 export class Default_claim_deposit_accounts_Tax {
     readonly page: Page
     //Tax Page Elements
+    readonly TaxRoundingDownRadioButton: Locator
+    readonly TaxRoundingUpRadioButton: Locator
+    readonly TaxRoundingOffRadioButton: Locator
     readonly TaxIncludeRadioButton: Locator
     readonly TaxExcludeRadioButton: Locator
     readonly SaveBtn: Locator
@@ -11,6 +14,9 @@ export class Default_claim_deposit_accounts_Tax {
     constructor(page: Page) {
         this.page = page        
         //Tax Page Elements
+        this.TaxRoundingDownRadioButton = this.page.getByRole('radio',{name: '切り捨て'})
+        this.TaxRoundingUpRadioButton = this.page.getByRole('radio',{name: '切り上げ'})
+        this.TaxRoundingOffRadioButton = this.page.getByRole('radio',{name: '四捨五入'})
         this.TaxIncludeRadioButton = this.page.getByRole('radio', { name: '税込' });
         this.TaxExcludeRadioButton = this.page.getByRole('radio', { name: '税抜' });
         this.SaveBtn = this.page.locator('button.el-button.btn-save');   
@@ -22,14 +28,29 @@ export class Default_claim_deposit_accounts_Tax {
         await this.page.getByRole('link', { name: '消費税' }).click();
         await this.page.waitForURL(baseURL+'/settings/tax_rounding_method/edit',{ timeout: 30000 });
     }
+    async SetTaxRoundingDown(){
+        await this.TaxRoundingDownRadioButton.click();
+        await this.SaveBtn.click();
+        await expect(this.TaxRoundingDownRadioButton).toBeChecked();
+    }
+    async SetTaxRoundingUp(){
+        await this.TaxRoundingUpRadioButton.click();
+        await this.SaveBtn.click();
+        await expect(this.TaxRoundingUpRadioButton).toBeChecked();
+    }
+    async SetTaxRoundingOff(){
+        await this.TaxRoundingOffRadioButton.click();
+        await this.SaveBtn.click();
+        await expect(this.TaxRoundingOffRadioButton).toBeChecked();
+    }
     async SetTaxInclude(){
         await this.TaxIncludeRadioButton.click();
         await this.SaveBtn.click();
-        await expect(this.TaxIncludeRadioButton).toBeEnabled();
+        await expect(this.TaxIncludeRadioButton).toBeChecked();
     }
     async SetTaxExclude(){
         await this.TaxExcludeRadioButton.click();
         await this.SaveBtn.click();
-        await expect(this.TaxExcludeRadioButton).toBeEnabled();
+        await expect(this.TaxExcludeRadioButton).toBeChecked();
     }    
 }
